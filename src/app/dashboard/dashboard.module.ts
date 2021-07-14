a9c2aae665ad0670889d267904adccb727896411
Gustavo Cotrim <gustavocotrim11@gmail.com>
2021-07-14 18:41:10 -0300

    Started the project and created the modules and components

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { RouterModule } from '@angular/router';
+import { StockCardComponent } from './stock-card/stock-card.component';
+
+@NgModule({
+  declarations: [DashboardPageComponent, StockCardComponent],
+  imports: [CommonModule],
+  exports: [DashboardPageComponent, StockCardComponent],
+})
+export class DashboardModule {}
